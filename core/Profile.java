1b691eb927329452c726ef3cc3325fabcbfa9540
e-greg <e.greg.bx@gmail.com>
2012-11-30 15:00:27 +0100

    first commit

---
+++ core/Profile.java
@@ -0,0 +1,22 @@
+package core;
+
+public class Profile
+{
+	private String name;
+
+	public Profile(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+}
---
+++ gui/JavaFXClient.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javafx.application.Application;
+
+public class JavaFXClient extends Application
+{
+	public static void main(String[] args)
+	{
+	    System.out.println("hello");// TODO Auto-generated method stub
+
+	}
+
+}
